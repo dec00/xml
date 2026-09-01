0ab85030e4963add7a833a9d5202ef6925c4a8bc
CyberHoe <sebastian.winning@gmail.com>
2026-09-01 16:13:25 +0200

    Add app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "de.cyberhoe.iptv"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "de.cyberhoe.iptv"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildFeatures { buildConfig = true }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.media3:media3-exoplayer:1.11.0")
+    implementation("androidx.media3:media3-exoplayer-hls:1.11.0")
+    implementation("androidx.media3:media3-ui:1.11.0")
+}
